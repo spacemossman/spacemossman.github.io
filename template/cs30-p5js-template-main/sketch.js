c5aa28e40d49b992e9647bc797b87520f425b32e
spacemossman <91297737+spacemossman@users.noreply.github.com>
2021-09-24 12:01:25 -0600

    added template

---
+++ template/cs30-p5js-template-main/sketch.js
@@ -0,0 +1,18 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  
+  
+  rect(mouseX, mouseY, 100, 50);
+}
