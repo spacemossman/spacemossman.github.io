ecae859cb7ac9cddfdc2809c66478bee6709f56f
spacemossman <91297737+spacemossman@users.noreply.github.com>
2021-10-26 12:11:31 -0600

    made text appear and added down arrow key

--- grid based game/sketch.js
+++ grid based game/sketch.js
@@ -10,10 +10,15 @@ let grid;
 let cellSize;
 let twentyfortyeight; 
 
+function preload(){
+
+}
 
 function setup() {
   createCanvas(windowWidth*0.5, windowHeight*0.5);
-  grid = createRandom2DArray(gridSize, gridSize);
+  grid = createEmpty2DArray(gridSize, gridSize);
+
+  cellSize = width/gridSize;
 }
 
 function draw() {
@@ -33,12 +38,17 @@ function displayGrid(){
       strokeWeight(1);
       rect(x*cellWidth, y * cellHeight, cellWidth, cellHeight);
 
-      fill("black");
-      textSize(cellSize*0.75);
-      textAlign(CENTER, CENTER);
-      text(grid[y][x], x*cellSize + cellSize/2, y*cellSize + cellSize/2);
-     
 
+      if(grid[y][x] !== 0){
+
+        fill("black");
+        textSize(cellSize*0.75);
+        textAlign(CENTER, CENTER);
+        text(grid[y][x], x*cellSize + cellSize/2, y*cellSize + cellSize/2);
+      }
+      
+      
+      
     }
   }
 }
@@ -48,16 +58,20 @@ function numbers2048(){
   if (keyIsPressed){
     for (let y = 0; y<gridSize; y++){
       for(let x = 0; x<gridSize; x++){
+        // up arrow key
         if(keyCode === 38 && grid[y][x] === grid[y + 1][x]){
           grid[y][x] = 0;
-          grid[y + 1][x] = grid[y+1][x]*grid[y+1];
+          grid[y - 1][x] = grid[y- 1][x]*grid[y -1];
         }
+        //
         else if (keyCode === 40 && grid[y][x] === grid[y][x +1]){
-
+          grid[y][x] = 0;
+          grid[y + 1][x] = grid[y+1][x]*grid[y+1][x];
 
         }
+      }
     }
-  }
+  } 
 }
 
 function createEmpty2DArray(rows, cols){
@@ -78,7 +92,12 @@ function createRandom2DArray(rows, cols){
   for (let y = 0; y<rows; y++){
     grid.push([]);
     for(let x = 0; x<cols; x++){
-      grid[y].push(0); 
+      if(random(100)< 20){
+        grid[y].push(1);
+      }
+      else{
+        grid[y].push[0];
+      }
     }
   }
   return grid;
